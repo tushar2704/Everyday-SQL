36b3eb87fd0d974eed50280bfc1f78d0aad46174
tushar27 <tushar.27041994@gmail.com>
2024-03-08 01:37:06 +0530

    basics

--- pages/page2.py
+++ pages/page2.py
@@ -70,115 +70,144 @@ def topics():
             )
     
     
-    # col1, col2 = st.columns([0.5, 0.5], gap="small")
+    col1, col2 = st.columns([0.5, 0.5], gap="small")
+    
+    with col1:
+        st.subheader("DDL - Data Definition Language")
+        
+        st.text(
+            """
+            Data Definition Language actually consists of the SQL commands that 
+            can be used to define the database schema.
+            It simply deals with descriptions of the database schema and is used 
+            to create and modify the structure of database objects in the database. 
+            DDL is a set of SQL commands used to create, modify, and delete database 
+            structures but not data. 
+            These commands are normally not used by a general user, who should be 
+            accessing the database via an application. 
+            """
+        )
+        if st.toggle("Show DDL Commands"):
+            st.code( 
+                    """
+                    CREATE: This command is used to create the database or its objects (like table, index, function, views, store procedure, and triggers).
+                    DROP: This command is used to delete objects from the database.
+                    ALTER: This is used to alter the structure of the database.
+                    TRUNCATE: This is used to remove all records from a table, including all spaces allocated for the records are removed.
+                    COMMENT: This is used to add comments to the data dictionary.
+                    RENAME: This is used to rename an object existing in the database.
+                    
+                    """, language="markdown")
+        
+        # if st.toggle("Show `st.write` sample output"):
+        #     st.write("Did you know I have more then 101 Supreme apps like this?")
+        
+        st.subheader("DQL (Data Query Language)")
+        
+        st.text(
+            """
+            DQL statements are used for performing queries on the data within schema objects. 
+            The purpose of the DQL Command is to get some schema relation based on the query passed to it. 
+            We can define DQL as follows it is a component of SQL statement that allows getting data from 
+            the database and imposing order upon it. It includes the SELECT statement. 
+            This command allows getting the data out of the database to perform operations with it. 
+            When a SELECT is fired against a table or tables the result is compiled into a 
+            further temporary table, which is displayed or perhaps received by the program i.e. a front-end.
+            """
+        )
+        if st.toggle("Show DQL Commands"):
+            st.code( 
+                    """
+                    SELECT: It is used to retrieve data from the database.
+                    """, language="markdown")
+            
+            
+            
+        st.subheader("DML- Data Manipulation Language")
+        
+        st.text(
+            """
+            The SQL commands that deal with the manipulation of data present in the database 
+            belong to DML or Data Manipulation Language and this includes most of the SQL statements. 
+            It is the component of the SQL statement that controls access to data and to the database. 
+            Basically, DCL statements are grouped with DML statements.
+            """
+        )
+        if st.toggle("Show DML Commands"):
+            st.code( 
+                    """
+                    INSERT: It is used to insert data into a table.
+                    UPDATE: It is used to update existing data within a table.
+                    DELETE: It is used to delete records from a database table.
+                    LOCK: Table control concurrency.
+                    CALL: Call a PL/SQL or JAVA subprogram.
+                    EXPLAIN PLAN: It describes the access path to data.
+                    """, language="markdown")
     
     
-    st.subheader("DDL - Data Definition Language")
     
-    st.text(
-        """
-        Data Definition Language actually consists of the SQL commands that 
-        can be used to define the database schema.
-        It simply deals with descriptions of the database schema and is used 
-        to create and modify the structure of database objects in the database. 
-        DDL is a set of SQL commands used to create, modify, and delete database 
-        structures but not data. 
-        These commands are normally not used by a general user, who should be 
-        accessing the database via an application. 
-        """
-    )
-    if st.toggle("Show DDL Commands"):
-        st.code( 
-                """
-                CREATE: This command is used to create the database or its objects (like table, index, function, views, store procedure, and triggers).
-                DROP: This command is used to delete objects from the database.
-                ALTER: This is used to alter the structure of the database.
-                TRUNCATE: This is used to remove all records from a table, including all spaces allocated for the records are removed.
-                COMMENT: This is used to add comments to the data dictionary.
-                RENAME: This is used to rename an object existing in the database.
-                
-                """, language="markdown")
-    
-    # if st.toggle("Show `st.write` sample output"):
-    #     st.write("Did you know I have more then 101 Supreme apps like this?")
-    
-    st.subheader("DQL (Data Query Language)")
-    
-    st.text(
-        """
-        DQL statements are used for performing queries on the data within schema objects. 
-        The purpose of the DQL Command is to get some schema relation based on the query passed to it. 
-        We can define DQL as follows it is a component of SQL statement that allows getting data from 
-        the database and imposing order upon it. It includes the SELECT statement. 
-        This command allows getting the data out of the database to perform operations with it. 
-        When a SELECT is fired against a table or tables the result is compiled into a 
-        further temporary table, which is displayed or perhaps received by the program i.e. a front-end.
-        """
-    )
-    if st.toggle("Show DQL Commands"):
-        st.code( 
-                """
-                SELECT: It is used to retrieve data from the database.
-                """, language="markdown")
+    with col2:
+        st.subheader("DCL - Data Control Language")
+        
+        st.markdown(
+            """
+            ##### To add text to the end of an existing file:
+            """
+        )
+        st.code(
+            """
+            with open('Tushar.txt', 'a') as file:
+                content = file.write("\n https://www.linkedin.com/in/tusharaggarwalinseec/")
+                print(content)
+            
+            """
+        )
         
         
+        st.subheader("TCL - Transaction Control Language")
         
-    st.subheader("DML- Data Manipulation Language")
-    
-    st.text(
-        """
-        The SQL commands that deal with the manipulation of data present in the database 
-        belong to DML or Data Manipulation Language and this includes most of the SQL statements. 
-        It is the component of the SQL statement that controls access to data and to the database. 
-        Basically, DCL statements are grouped with DML statements.
-        """
-    )
-    if st.toggle("Show DML Commands"):
-        st.code( 
-                """
-                INSERT: It is used to insert data into a table.
-                UPDATE: It is used to update existing data within a table.
-                DELETE: It is used to delete records from a database table.
-                LOCK: Table control concurrency.
-                CALL: Call a PL/SQL or JAVA subprogram.
-                EXPLAIN PLAN: It describes the access path to data.
-                """, language="markdown")
+        st.markdown(
+            """
+            ##### To read a file line by line into a list:
+            """
+        )
+        st.code(
+            """
+            with open('Tushar.txt', 'r') as file:
+                content = file.readlines()
+                print(content)
+            
+            """
+        )
     
     
     
     
-    st.subheader("DCL - Data Control Language")
-    
-    st.markdown(
-        """
-        ##### To add text to the end of an existing file:
-        """
-    )
-    st.code(
-        """
-        with open('Tushar.txt', 'a') as file:
-            content = file.write("\n https://www.linkedin.com/in/tusharaggarwalinseec/")
-            print(content)
-        
-        """
-    )
     
     
-    st.subheader("TCL - Transaction Control Language")
     
-    st.markdown(
-        """
-        ##### To read a file line by line into a list:
-        """
-    )
-    st.code(
-        """
-        with open('Tushar.txt', 'r') as file:
-            content = file.readlines()
-            print(content)
-        
-        """
-    )
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ###########################################################################################
+    # Basics to Advanced Commands
+    ###########################################################################################
     
     
     st.subheader("SQL - Beginner SQL Syntax")
@@ -186,14 +215,12 @@ def topics():
     
     st.markdown(
         """
-        ##### To process each line in a file:
+        
         """
     )
     st.code(
         """
-        with open('Tushar.txt', 'a') as file:
-            for line in file:
-                print(line.strip())
+        
         
         """
     )
@@ -204,36 +231,28 @@ def topics():
     
     st.markdown(
         """
-        ##### To check if a file exists before performing file operations:
+        
         """
     )
     st.code(
         """
-        import os
-        if os.path.exists("Tushar.txt"):
-            print("File exists.")
-        else:
-            print("File does not exists.")
+       
         """
     )
     
-    # if st.toggle("Show `st.write` sample output"):
-    #     st.write("Did you know I have more then 101 Supreme apps like this?")
+    
     
     
     st.subheader("WHERE")
     
     st.markdown(
         """
-        ##### To write each element of a list to a new line in a file:
+        
         """
     )
     st.code(
         """
-        lines = ['First line', 'Second line', 'Third line']
-        with open("Tushar.txt","w") as file:
-            for line in lines:
-                file.write(f'{file}\n')           
+                 
         """
     )
     
@@ -244,16 +263,12 @@ def topics():
     
     st.markdown(
         """
-        ##### To work with multiple files simultaneously using `with` blocks:
+        
         """
     )
     st.code(
         """
-        with open("source.txt","r") as source,
-            open("destination.txt","w") as destination
-            
-            content = source.read()
-            destination.write(content)
+        
         """
     )
     
@@ -262,17 +277,12 @@ def topics():
     
     st.markdown(
         """
-        ##### To safely delete a file if it exists:
+        
         """
     )
     st.code(
         """
-        import os
-        if os.path.exists("Tushar.txt"):
-            os.remove("Tushar.txt")
-            print("File deleted.")
-        else:
-            print("File does not exist.")
+        
         """
     )
     
@@ -282,17 +292,12 @@ def topics():
     
     st.markdown(
         """
-        ##### To read from and write to a file in binary mode (useful for images, videos,etc.):
+       
         """
     )
     st.code(
         """
-        # Reading a binary file
-        with open('image.jpg','rb') as file:
-            content = file.read()
-        #Writing to a binary file
-        with open('copy.jpg','wb') as file:
-            file.write(content)
+        
         """
     )
         
